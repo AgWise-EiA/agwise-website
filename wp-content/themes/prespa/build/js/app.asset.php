23ebcbba34c16369574a3c1bc730033c5626ff06
Akilimo Machine <s.barasa@cgiar.org>
2024-06-20 09:18:17 +0000

    refactor: revised themes

--- wp-content/themes/prespa/build/js/app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '93ff60f2282ffd85d022');
--- wp-content/themes/prespa/build/js/woocommerce.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c0aff99d8352683ee782');
