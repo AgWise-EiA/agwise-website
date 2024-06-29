c696c29ec9c0e1d577ae217e2d90da9c01fc0989
Akilimo Machine <s.barasa@cgiar.org>
2024-06-29 16:16:06 +0000

    fix: system cleanup

--- wp-content/plugins/chatbot-chatgpt/assets/js/greetings.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function ($) {
-    localStorage.setItem('chatbot_chatgpt_initial_greeting', greetings_data.initial_greeting);
-    localStorage.setItem('chatbot_chatgpt_subsequent_greeting', greetings_data.subsequent_greeting);
-});
